93935a62b23d7017215d93496df1b9411d496ba0
BtnPresser <38516254+BtnPresser@users.noreply.github.com>
2018-09-17 18:04:06 -0400

    Go back, and commit a hello_world for convention.... and a linux push instead of windows

---
+++ basics/hello.py
@@ -0,0 +1,15 @@
+# This is a introdutory program that doesn't really do anything in particular...
+# It also does the basic saying hello to a person thing
+
+def print_hello():
+    print("Hello World\n")
+
+
+def ask_name():
+    inputName = input("What is your name? ")
+    print("Hello {0}!".format(inputName))
+
+
+if __name__ == "__main__":
+    print_hello()
+    ask_name()
